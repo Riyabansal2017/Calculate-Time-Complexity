8122209d905b2eeb2c464294faf0cc6741e3d9b9
Riyabansal2017 <32256171+Riyabansal2017@users.noreply.github.com>
2021-10-01 21:30:33 +0530

    Update time.c

--- time.c
+++ time.c
@@ -5,30 +5,30 @@
 int main()
 {
 	clock_t t;
-	int a[16000]={0},i;
-	t=clock();
+	int a[16000] = {0},i;
+	t = clock();
 	srand(time(0));
-	for(i=0;i<16000;i++)
+	for(i = 0; i < 16000; i++)
 	{
-		a[i]=rand();
-		//printf("%d %d  ",i,a[i]);
+		a[i] = rand();
+		printf("%d %d \n", i, a[i]);
 	}
 	int j;
-	for(i=0;i<=1599;i++)
+	for(i = 0; i <= 1599; i++)
 	{
-	for(j=0;j<=1599-i;j++)
+		for(j = 0; j <= 1599 - i; j++)
 		{
-			if(a[j]>a[j+1])
+			if(a[j] > a[j+1])
 			{
 				int y;
-				y=a[j+1];
-				a[j+1]=a[j];
-				a[j]=y;
+				y = a[j+1];
+				a[j+1] = a[j];
+				a[j] = y;
 			}
 		}
 	}
-	t=clock()-t;
-	printf("%lu",t);
+	t = clock() - t;
+	printf("%lu", t);
 	return 0;
 }
 
